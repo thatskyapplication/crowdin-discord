a82613502c0e537aae3e4a0c7be7626e4be29e94
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-11-23 20:26:19 +0000

    feat: Version worker

--- source/index.ts
+++ source/index.ts
@@ -15,55 +15,63 @@ interface Env {
 	WEBHOOK_ID: string;
 	WEBHOOK_TOKEN: string;
 	SENTRY_DATA_SOURCE_NAME: string;
+	CF_VERSION_METADATA: WorkerVersionMetadata;
 }
 
-export default withSentry((env) => ({ dsn: env.SENTRY_DATA_SOURCE_NAME, sendDefaultPii: true }), {
-	async fetch(request, env) {
-		if (request.method !== "POST") {
-			return new Response(null, { status: 405 });
-		}
+export default withSentry(
+	(env) => ({
+		dsn: env.SENTRY_DATA_SOURCE_NAME,
+		release: env.CF_VERSION_METADATA.id,
+		sendDefaultPii: true,
+	}),
+	{
+		async fetch(request, env) {
+			if (request.method !== "POST") {
+				return new Response(null, { status: 405 });
+			}
 
-		const authorisation = request.headers.get("Authorization");
+			const authorisation = request.headers.get("Authorization");
 
-		if (authorisation !== env.CROWDIN_TOKEN) {
-			return new Response(null, { status: 401 });
-		}
+			if (authorisation !== env.CROWDIN_TOKEN) {
+				return new Response(null, { status: 401 });
+			}
 
-		const data = (await request.json()) as Events;
-		let components: APIMessageTopLevelComponent[];
+			const data = (await request.json()) as Events;
+			let components: APIMessageTopLevelComponent[];
 
-		switch (data.event) {
-			case "file.translated":
-			case "file.approved":
-			case "file.added":
-			case "file.updated":
-			case "file.reverted":
-			case "file.deleted":
-				components = await createFileComponents(data, env.CROWDIN_WEBHOOK_TOKEN);
-				break;
-			case "project.translated":
-			case "project.approved":
-				components = createProjectTranslatedAndApprovedComponents(data);
-				break;
-			case "project.built":
-				components = await createProjectBuiltComponents(data, env.CROWDIN_WEBHOOK_TOKEN);
-				break;
-			case "suggestion.added":
-			case "suggestion.updated":
-			case "suggestion.deleted":
-			case "suggestion.approved":
-			case "suggestion.disapproved":
-				components = createSuggestionComponents(data);
-				break;
-		}
+			switch (data.event) {
+				case "file.translated":
+				case "file.approved":
+				case "file.added":
+				case "file.updated":
+				case "file.reverted":
+				case "file.deleted":
+					components = await createFileComponents(data, env.CROWDIN_WEBHOOK_TOKEN);
+					break;
+				case "project.translated":
+				case "project.approved":
+					components = createProjectTranslatedAndApprovedComponents(data);
+					break;
+				case "project.built":
+					components = await createProjectBuiltComponents(data, env.CROWDIN_WEBHOOK_TOKEN);
+					break;
+				case "suggestion.added":
+				case "suggestion.updated":
+				case "suggestion.deleted":
+				case "suggestion.approved":
+				case "suggestion.disapproved":
+					components = createSuggestionComponents(data);
+					break;
+			}
 
-		await new API(new REST()).webhooks.execute(env.WEBHOOK_ID, env.WEBHOOK_TOKEN, {
-			allowed_mentions: { parse: [] },
-			components,
-			flags: MessageFlags.IsComponentsV2,
-			with_components: true,
-		});
+			await new API(new REST()).webhooks.execute(env.WEBHOOK_ID, env.WEBHOOK_TOKEN, {
+				allowed_mentions: { parse: [] },
+				components,
+				flags: MessageFlags.IsComponentsV2,
+				with_components: true,
+			});
 
-		return new Response(null, { status: 204 });
-	},
-} satisfies ExportedHandler<Env>);
+			return new Response(null, { status: 204 });
+		},
+	} satisfies ExportedHandler<Env>,
+);
